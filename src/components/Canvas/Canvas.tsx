ec9fa70b61bc3ddc4fad62a183eeb6b9b4875b0b
Игорь <chegs@bk.ru>
2026-02-24 22:54:39 +0300

    Set selection outline to 0.42mm

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -2055,7 +2055,7 @@ const CanvasElement: React.FC<CanvasElementProps> = ({ element, isPreview, isSel
             d={d}
             fill="none"
             stroke="#007acc"
-            strokeWidth={0.5}
+            strokeWidth={0.42}
             vectorEffect="non-scaling-stroke"
           />
         </>
